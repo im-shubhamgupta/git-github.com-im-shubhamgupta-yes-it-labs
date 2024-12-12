ddce11c140f6285c5bb9174e94cab6965bddf917
Shubham kumar Gupta <shubhamgupta309@gmail.com>
2024-12-12 23:49:00 +0530

    indent

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -103,7 +103,7 @@
                 <h5 class="modal-title" id="exampleModalLabel">Find Distance by Latitude and Longitude</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                </button>
+                </button>  
             </div>
             <div class="modal-body">
                 <div class="">
